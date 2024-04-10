98b6085ef00ac8ecc23149297707a44ba2a36432
huangkai <h1770360848@outlook.com>
2024-04-10 21:06:35 +0800

    feat(lab): finish lab1

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -2,5 +2,20 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if (head == NULL) {
+        return 0;
+    }
+
+    node *fast = head;
+    node *slow = head;
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (fast == slow) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
