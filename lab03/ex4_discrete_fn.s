ca6804eb66aa568d057f9063e46a5e0ada766cdd
huangkai <h1770360848@outlook.com>
2024-04-14 15:19:38 +0800

    feat(lab): lab3 exercise 4

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -5,7 +5,10 @@
 # a1 is the address of the "output" array (defined above).
 # The return value should be stored in a0
 f:
-    # Your code here
+    addi a0, a0, 3 # according to Hint 4, f(-3) should be stored at offset 0
+    slli a0, a0, 2 # multiply the index by the size of the elements of the array
+    add a1, a1, a0 # add offset to the address of the array 
+    lw a0, 0(a1)
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
