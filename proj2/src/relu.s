2b29d5dc8a5b484ff0ee400c1c4cc47328e71ae4
huangkai <h1770360848@outlook.com>
2024-04-17 20:17:59 +0800

    feat(project): project1 part A Task 2

--- proj2/src/relu.s
+++ proj2/src/relu.s
@@ -14,25 +14,25 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    bgt a1, zero, loop_start # make sure the length of the array is greater than 0
+    # raise exception if length less than 1
+    li a0, 36
+    j exit
 
 loop_start:
+    beq a1, zero, loop_end
+    addi a1, a1, -1
+    # load value from address a0: t0 = *a0
+    lw t0, 0(a0)
 
-
-
-
-
-
-
+    bge t0, zero, loop_continue
+    # sets negative value in the array to 0
+    sw zero, 0(a0)
 
 loop_continue:
-
-
+    addi, a0, a0, 4
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
-
-
     jr ra
