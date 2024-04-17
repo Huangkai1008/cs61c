c366a7389dc8bb9c2d0efe7c471cacedb95ce798
huangkai <h1770360848@outlook.com>
2024-04-17 21:57:07 +0800

    feat(project): project1 part A Task 4

--- proj2/src/dot.s
+++ proj2/src/dot.s
@@ -18,27 +18,46 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
     # Prologue
-
+    ble a2, zero, wrong_elements_e
+    ble a3, zero, wrong_stride_e
+    ble a4, zero, wrong_stride_e
 
 loop_start:
+    # initialize final result to 0
+    li t0, 0
+    # get byte offset of arr0
+    li t1, 4
+    mul t1, t1, a3
+    # get byte offset of arr1
+    li t2, 4
+    mul t2, t2, a4
 
+loop_continue:
+    beq a2, zero, loop_end
+    # get value to product in arr0
+    lw t3, 0(a0)
+    # get value to product in arr1
+    lw t4, 0(a1)
+    mul t3, t3, t4
+    # add product value to result
+    add t0, t0, t3
 
+    addi a2, a2, -1
+    add a0, a0, t1
+    add a1, a1, t2
 
-
-
-
-
-
-
-
-
+    j loop_continue
 
 loop_end:
-
-
     # Epilogue
-
-
+    mv a0, t0
     jr ra
+
+wrong_elements_e:
+    li a0, 36
+    j exit
+
+wrong_stride_e:
+    li a0, 37
+    j exit
