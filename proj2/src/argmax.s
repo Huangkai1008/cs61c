7784ed78d3e84e070948896dd8dc5f721de43d12
huangkai <h1770360848@outlook.com>
2024-04-17 21:19:03 +0800

    feat(project): project1 part A Task 3

--- proj2/src/argmax.s
+++ proj2/src/argmax.s
@@ -16,15 +16,34 @@
 # =================================================================
 argmax:
     # Prologue
-
+    bgt a1, zero, loop_start # make sure the length of the array is greater than 0
+    # raise exception if length less than 1
+    li a0, 36
+    j exit
 
 loop_start:
-
+    # initialize t0 to 0
+    li t0, 0
+    # initialize t1 as max value
+    lw t1, 0(a0)
+    # initialize t2 to 1
+    li t2, 1
 
 loop_continue:
+    beq t2, a1, loop_end
+    addi a0, a0, 4
+    lw t3, 0(a0)
+    bgt t3, t1, update_max
+    addi t2, t2, 1
+    j loop_continue
 
+update_max:
+    mv t0, t2
+    mv t1, t3
+    addi t2, t2, 1
+    j loop_continue
 
 loop_end:
     # Epilogue
-
+    mv a0, t0
     jr ra
