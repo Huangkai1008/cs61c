88bf30f36b03bbcc4a175247a52d29150d8cb54c
huangkai <h1770360848@outlook.com>
2024-04-18 16:58:30 +0800

    feat(project): project1 part B Task 8

--- proj2/src/write_matrix.s
+++ proj2/src/write_matrix.s
@@ -23,18 +23,92 @@
 #     this function terminates the program with error code 30
 # ==============================================================================
 write_matrix:
-
     # Prologue
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp) # hold the file descriptor
+    sw s1, 8(sp) # hold pointer to the matrix in memory
+    sw s2, 12(sp) # hold the number of rows in the matrix
+    sw s3, 16(sp) # hold the number of columns in the matrix
 
+    mv s1, a1
+    mv s2, a2
+    mv s3, a3
 
+# open the file with write permissions
+open_file:
+    li a1, 1
+    jal fopen
+    li t0, -1
+    beq a0, t0 fopen_error
+    mv s0, a0 # file descriptor
 
+    li t0, -1
+    beq a0, t0, fopen_error
+    mv s0, a0
 
+# write the number of rows and columns to the file
+write_file:
+    # write row number
+    addi sp, sp, -4
+    sw s2, 0(sp)
+    mv a1, sp
+    li a2, 1
+    li a3, 4
+    jal fwrite
+    li t0 1
+    bne a0, t0, fwrite_error
 
+    # write column number
+    sw s3, 0(sp)
+    mv a0, s0
+    mv a1, sp
+    li a2, 1
+    li a3, 4
+    jal fwrite
+    li t0 1
+    bne a0, t0, fwrite_error
+    addi sp, sp, 4
 
+    # write data
+    mv a0, s0
+    mv a1, s1
+    mv a2, s2
+    mul a2, a2, s3
+    li a3, 4
+    jal fwrite
+    mv t0 s2
+    mul t0 s2 s3
+    bne a0, t0, fwrite_error
 
+# close a file, saving any writes we have made to the file
+close_file:
+    mv a0, s0
+    jal fclose
+    bne a0, zero, fclose_error
 
-
+end:
     # Epilogue
-
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 20
     jr ra
+
+fopen_error:
+    li a0, 27
+    j exit
+
+fread_error:
+    li a0, 29
+    j exit
+
+fwrite_error:
+    li a0, 30
+    j exit
+
+fclose_error:
+    li a0, 28
+    j exit
